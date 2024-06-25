90b31e8ce29d78644091d4382e7b97b4cfad84cc
simonlotzkar <simonlotzkar@gmail.com>
2024-06-25 08:45:52 -0700

    added attacking, healthbars and death mechanics

--- public/js/client.js
+++ public/js/client.js
@@ -31,46 +31,67 @@ window.addEventListener("load", function(){
 
     // Represents a soldier NPC that is either a player or enemy.
     class Soldier {
-        constructor(side, strength, vitality, agility) {
-            this.side = side; // player or enemy
-            this.width = 200;
-            this.height = 200;
+        constructor(isPlayer, strength, vitality, agility, range, spriteSource) {
+            this.isPlayer = isPlayer;
+            this.strength = strength;
+            this.vitality = vitality;
+            this.agility = agility;
+            this.range = range;
+        
+            this.width = 100;
+            this.height = 100;
+            this.sourceWidth = 32;
+            this.sourceHeight = 32;
             this.y = gameHeight - this.height;
-            this.image = document.getElementById("soldierImage");
+            this.image = document.getElementById(spriteSource);
 
             this.frame = 0;
             this.maxFrame = 4;
-            this.fps = 20;
+            this.fps = 15;
             this.timeOnFrame = 0;
 
-            this.maxHP = vitality;
+            this.maxHP = (this.vitality * 2);
             this.hp = this.maxHP;
-            this.speed = agility;
+            this.speed = (this.agility * 0.75);
 
-            // initial spawn location
-            if (this.side == "player") {
-                this.x = 10;
-            } else if (this.side == "enemy") {
-                this.x = gameWidth - this.width - 10;
-                this.speed *= -1;
+            this.moveable = true;
+            this.target = null;
+            this.timeOnAttack = 0;
+            this.attackInterval = (5000 / this.agility);
+            this.rangeWidth = this.range * this.width;
+            this.rangeX = this.x;
+
+            if (this.isPlayer) {
+                this.x = 10; // spawn at the left of the map
             } else {
-                console.error("Soldier is neither player nor enemy.");
+                this.x = gameWidth - this.width - 10; // spawn at the right of the map
+                this.speed *= -1; // reverse the speed direction if enemy
+                this.rangeX = this.x - (this.width * (this.range - 1)); // offset the rangeBox to the left if enemy
             }
         }
         draw(context) {
+            // Draw sprite 
+            // TODO: Make a flipped source file for each soldier and draw from that if 'enemy'
+            context.drawImage(
+                this.image,                    // image
+                this.frame * this.sourceWidth, // source x
+                0,                             // source y
+                this.sourceWidth,              // source width
+                this.sourceHeight,             // source height
+                this.x,                        // x
+                this.y,                        // y
+                this.width,                    // width 
+                this.height);                  // height
+    
+            // Hitbox outline
             context.strokeStyle = "white";
             context.strokeRect(this.x, this.y, this.width, this.height);
-            context.drawImage(
-                this.image,                // image
-                this.frame * this.width,   // source x
-                0,                         // source y
-                this.width,                // source width
-                this.height,               // source height
-                this.x,                    // x
-                this.y,                    // y
-                this.width,                // width 
-                this.height);              // height
 
+            // Rangebox outline
+            context.strokeStyle = "red";
+            context.strokeRect(this.rangeX, this.y, this.rangeWidth, this.height);
+
+            // Healthbar background
             context.fillStyle = "red";
             context.fillRect(
                 this.x, 
@@ -78,6 +99,7 @@ window.addEventListener("load", function(){
                 this.width, 
                 (this.height / 10));
 
+            // Healthbar foreground
             context.fillStyle = "green";
             context.fillRect(
                     this.x, 
@@ -85,29 +107,75 @@ window.addEventListener("load", function(){
                     this.width * (this.hp / this.maxHP), 
                     (this.height / 10));
 
+            // Healthbar label
             context.fillStyle = "white";
-            context.font = "20px Helvetica";
+            context.font = "15px Helvetica";
             context.fillText(this.hp + "/" + this.maxHP, this.x, this.y);
         }
-        update(timestamp) {
-            // Animate movement
-            if (this.timeOnFrame > (1000 / this.fps)) {
-                if (this.frame >= this.maxFrame) {
-                    this.frame = 0;
+        update(timeSinceLastUpdate, soldiers) {
+            // Check if attacking
+            if (this.target != null) {
+                this.moveable = false;
+                if (this.timeOnAttack >= this.attackInterval) {
+                    this.timeOnAttack = 0;
+                    if (this.target.hp <= this.strength) {
+                        this.target.hp = 0;
+                    } else if (!(this.target.hp <= 0)) {
+                        this.target.hp -= this.strength;
+                    }
                 } else {
-                    this.frame++;
+                    // dont damage
                 }
-                this.timeOnFrame = 0;
+                this.timeOnAttack += timeSinceLastUpdate;
             } else {
-                this.timeOnFrame += timestamp;
+                // Check if in range of enemies
+                soldiers.forEach(soldier => {
+                    if (!(soldier === this)) {
+                        let hitBox = {x: soldier.x - cameraX, y: soldier.y, width: soldier.width, height: soldier.height};
+                        let rangeBox = {x: this.rangeX - cameraX, y: this.y, width: this.rangeWidth, height: this.height};
+
+                        if (hitBox.x < rangeBox.x + rangeBox.width &&
+                            hitBox.x + hitBox.width > rangeBox.x) {
+                            // Collision detected
+                            this.target = soldier;
+                        } else {
+                            // No collision
+                            this.target = null;
+                        }
+                    }
+                });
             }
 
-            // Movement
+            // check if at map edge
             let relativeX = this.x - cameraX;
             if (relativeX >= (gameWidth - this.width) || relativeX <= 0) {
-                this.x += cameraSpeed;
-            } else {
+                this.moveable = false;
+            }
+
+            if (this.moveable) {
+                // Animate movement
+                if (this.timeOnFrame > (1000 / this.fps)) {
+                    if (this.frame >= this.maxFrame) {
+                        this.frame = 0;
+                    } else {
+                        this.frame++;
+                    }
+                    this.timeOnFrame = 0;
+                } else {
+                    this.timeOnFrame += timeSinceLastUpdate;
+                }
+                // Move soldier
                 this.x += this.speed + cameraSpeed;
+            } else {
+                this.frame = 0;
+                this.x += cameraSpeed;
+            }
+
+            // Flip rangebox if enemy
+            if (this.isPlayer) {
+                this.rangeX = this.x;
+            } else {
+                this.rangeX = this.x - (this.width * (this.range - 1));
             }
         }
     }
@@ -148,9 +216,10 @@ window.addEventListener("load", function(){
     }
 
     const input = new InputHandler();
-    const soldier0 = new Soldier("player", 1, 10, 3);
-    const soldier1 = new Soldier("enemy", 1, 10, 3);
-    const soldiers = [soldier0, soldier1];
+    // soldier(isPlayer, strength, vitality, agility, range, sourceImage)
+    const soldier0 = new Soldier(true, 2, 2, 5, 5, "soldierImage");
+    const soldier1 = new Soldier(false, 5, 5, 2, 2, "soldierImage");
+    let soldiers = [soldier0, soldier1];
     const background = new Background();
 
     let food = 10;
@@ -170,10 +239,13 @@ window.addEventListener("load", function(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         background.draw(ctx);
         background.update(input);
-        for (let i = 0; i < soldiers.length; i++) {
-            soldiers[i].draw(ctx);
-            soldiers[i].update(deltaTimestamp);
-        }
+
+        soldiers.forEach(soldier => {
+            soldier.draw(ctx);
+            soldier.update(deltaTimestamp, soldiers);
+        });
+        soldiers = soldiers.filter(soldier => soldier.hp > 0);
+
         displayUI(ctx);
         requestAnimationFrame(animate);
     }
